c603b98ef672726a358986609a0cf970c5fc1288
lehoangvust2523013 <vum2523013@gstudent.ctu.edu.vn>
2024-03-16 09:05:20 +0700

    day file

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Phuong Thanh Tri xin chao moi nguoi</h1>');
+  res.end('<h1>Panh Trí hỏi các bạn chiều nay có nhậu không cho anh hay</h1>');
+
+  
 });
 
 server.listen(port,() => {
